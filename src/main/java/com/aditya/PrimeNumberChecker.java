813e2a1f2826a71a17049be281ceed5541a72d0e
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-09-05 23:33:03 +0530

    created method to check Prime Number PrimeNumberChecker.java class

--- src/main/java/com/aditya/PrimeNumberChecker.java
+++ src/main/java/com/aditya/PrimeNumberChecker.java
@@ -6,5 +6,23 @@
 package com.aditya;
 
 public class PrimeNumberChecker {
-
+    /**
+     * This method checks if the given number is prime.
+     *
+     * @param number the number to check.
+     * @return true if the number is prime, false otherwise.
+     */
+    public boolean isPrime(int number) {
+        if(number>1){
+            int counter = 0;
+            for (int i = 2; i < number; i++) {
+                if (number % i == 0) {
+                    counter = 1;
+                    break;
+                }
+            }
+            return counter == 0;
+        }
+        else return false;
+    }
 }
